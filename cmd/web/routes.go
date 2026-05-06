f094ffb70e78149739460ed17e88a639f0f0e245
Raghav Khandelwal <raaghav.khandelwal@gmail.com>
2026-05-06 15:37:22 -0700

    tls support

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -15,7 +15,7 @@ func (app *application) routes() http.Handler {
 	})
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	router.Handler(http.MethodGet, "/static/*filepath", fileServer)
+	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
 
 	dynamic := alice.New(app.sessionManager.LoadAndSave)
 
